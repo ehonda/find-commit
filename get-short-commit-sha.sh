1d2b95ff26cdb0aa3df894700ac36c2a3080cd8e
Dennis Renz <dennis@r-renz.de>
2023-06-01 23:48:59 +0200

    Fix termination condition

--- get-short-commit-sha.sh
+++ get-short-commit-sha.sh
@@ -14,7 +14,8 @@ commit_sha_matches() {
   local regex=$1
   local commit_sha=$2
   # Use machine locale for consistent grepping
-  LC_ALL=C echo "$commit_sha" | grep -qE "$regex"
+  LC_ALL=C echo -n "$commit_sha" | grep -qE "$regex"
+  echo $? # Return the exit code of grep
 }
 
 # Make commits until the commit SHA matches the given regex
@@ -25,7 +26,7 @@ randomize_commit_sha() {
   echo "$commit_sha"
 
   # Use grep instead of =~ for a more sane regex experience
-  while [[ ! $(commit_sha_matches regex commit_sha) ]]; do
+  while [[ $(commit_sha_matches regex commit_sha) -ne 0 ]]; do
     create_commit
     commit_sha=$(get_short_commit_sha)
     echo "$commit_sha"
