a3bd152f2df8dcf850daed33bcf7b7b937ae4b80
Dennis Renz <dennis@r-renz.de>
2023-06-01 23:15:37 +0200

    Fix matching

--- get-short-commit-sha.sh
+++ get-short-commit-sha.sh
@@ -10,6 +10,13 @@ create_commit() {
   git commit --allow-empty -m "Randomize commit SHA"
 }
 
+commit_sha_matches() {
+  local regex=$1
+  local commit_sha=$2
+  # Use machine locale for consistent grepping
+  LC_ALL=C echo "$commit_sha" | grep -qE "$regex"
+}
+
 # Make commits until the commit SHA matches the given regex
 randomize_commit_sha() {
   local regex=$1
@@ -17,7 +24,8 @@ randomize_commit_sha() {
   commit_sha=$(get_short_commit_sha)
   echo "$commit_sha"
 
-  while [[ ! "$commit_sha" =~ $regex ]]; do
+  # Use grep instead of =~ for a more sane regex experience
+  while ! [[ $(commit_sha_matches regex commit_sha) ]]; do
     create_commit
     commit_sha=$(get_short_commit_sha)
     echo "$commit_sha"
