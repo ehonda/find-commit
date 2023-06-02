7cf631d8652735d61facd62b50b0a9282cd13430
Dennis Renz <dennis@r-renz.de>
2023-06-02 20:39:45 +0200

    Fix while condition

--- get-short-commit-sha.sh
+++ get-short-commit-sha.sh
@@ -13,9 +13,6 @@ create_commit() {
 commit_sha_matches() {
   local regex=$1
   local commit_sha=$2
-  # Debug
-  echo "regex: $regex"
-  echo "checked commit_sha: $commit_sha"
   # Use machine locale for consistent grepping
   LC_ALL=C echo -n "$commit_sha" | grep -qE "$regex"
   echo $? # Return the exit code of grep
@@ -23,15 +20,15 @@ commit_sha_matches() {
 
 # Make commits until the commit SHA matches the given regex
 randomize_commit_sha() {
-  local regex=$1
+  local regex="$1"
   local commit_sha
-  commit_sha=$(get_short_commit_sha)
+  commit_sha="$(get_short_commit_sha)"
   echo "$commit_sha"
 
   # Use grep instead of =~ for a more sane regex experience
-  while [[ "$(commit_sha_matches regex commit_sha)" -ne 0 ]]; do
+  while [[ $(commit_sha_matches "$regex" "$commit_sha") -ne 0 ]]; do
     create_commit
-    commit_sha=$(get_short_commit_sha)
+    commit_sha="$(get_short_commit_sha)"
     echo "$commit_sha"
   done
 }
