b5e853fedd1e62d25af3e484c62080dbf516fa6e
Dennis Renz <dennis@r-renz.de>
2023-06-02 20:35:29 +0200

    debug

--- get-short-commit-sha.sh
+++ get-short-commit-sha.sh
@@ -13,6 +13,9 @@ create_commit() {
 commit_sha_matches() {
   local regex=$1
   local commit_sha=$2
+  # Debug
+  echo "regex: $regex"
+  echo "checked commit_sha: $commit_sha"
   # Use machine locale for consistent grepping
   LC_ALL=C echo -n "$commit_sha" | grep -qE "$regex"
   echo $? # Return the exit code of grep
@@ -26,7 +29,7 @@ randomize_commit_sha() {
   echo "$commit_sha"
 
   # Use grep instead of =~ for a more sane regex experience
-  while [[ $(commit_sha_matches regex commit_sha) -ne 0 ]]; do
+  while [[ "$(commit_sha_matches regex commit_sha)" -ne 0 ]]; do
     create_commit
     commit_sha=$(get_short_commit_sha)
     echo "$commit_sha"
