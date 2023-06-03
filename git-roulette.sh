0a317434205f2c1effcb1d25d49c665a836c5672
Dennis Renz <dennis@r-renz.de>
2023-06-03 23:25:26 +0200

    Rename main script and add predefined pattern

--- git-roulette.sh
+++ git-roulette.sh
@@ -27,6 +27,7 @@ randomize_commit_sha() {
   commit_sha="$(get_short_commit_sha)"
   echo "$commit_sha"
 
+  # TODO: Reevaluate, maybe we do want to use =~
   # Use grep instead of =~ for a more sane regex experience
   while [[ $(commit_sha_matches "$regex" "$commit_sha") -ne 0 ]]; do
     create_commit
@@ -35,5 +36,23 @@ randomize_commit_sha() {
   done
 }
 
+from_predefined_patter_or_user_input() {
+  local user_input="$1"
+  # switch case over provided user input do determine whether to transform it or take it literally
+  case "$user_input" in
+    "natural number (only digits)" | "nat digits" | n | 0 )
+      echo "^[0-9]+$"
+      ;;
+    
+    "natural number (optional e notation)" | "nat e" | ne | 0e0 )
+      echo "^[0-9]+e{0,1}[0-9]+$"
+      ;;
+
+    * )
+      echo "$user_input"
+      ;;
+  esac
+}
+
 # Run the function with the given regex
-randomize_commit_sha "$1"
+randomize_commit_sha "$(from_predefined_patter_or_user_input "$1")"
